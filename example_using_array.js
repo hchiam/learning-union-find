34f8e40ae5d2a7e6df0a7100034a4fbe2a7ef97e
hchiam <hchiam@users.noreply.github.com>
2022-02-15 22:49:12 -0500

    fix constructor. rename find to findSetOf. add isInSameSet. add exampleUsage

--- example_using_array.js
+++ example_using_array.js
@@ -2,19 +2,19 @@
 
 class UnionFind {
   constructor(size) {
-    this.parentOf = new Array(size).map((x, i) => i);
-    this.sizeOf = new Array(size).map((x) => 1);
+    this.parentOf = new Array(size).fill(null).map((x, i) => i);
+    this.sizeOf = new Array(size).fill(null).map((x) => 1);
   }
 
-  find(x) {
+  findSetOf(x) {
     if (this.parentOf[x] !== x) {
-      this.parentOf[x] = this.find(this.parentOf[x]);
+      this.parentOf[x] = this.findSetOf(this.parentOf[x]);
     }
-    return this.parentOf[x];
+    return this.parentOf[x]; // if it's its own parent, or if parent is found
   }
 
   union(x, y) {
-    let [smallParent, bigParent] = [this.find(x), this.find(y)];
+    let [smallParent, bigParent] = [this.findSetOf(x), this.findSetOf(y)];
     if (smallParent === bigParent) return smallParent;
 
     const namedWrongly = this.sizeOf[smallParent] > this.sizeOf[bigParent];
@@ -28,4 +28,37 @@ class UnionFind {
 
     return bigParent;
   }
+
+  isInSameSet(x, y) {
+    return this.findSetOf(x) === this.findSetOf(y);
+  }
+}
+
+exampleUsage();
+
+function exampleUsage() {
+  const data = [0, 1, 0, 1];
+
+  const uf = new UnionFind(data.length);
+
+  for (let i = 0; i < data.length; i++) {
+    uf.union(data[i], i);
+  }
+
+  console.log(uf);
+  data.forEach((x) => {
+    console.log(`${x} is under group`, uf.findSetOf(x));
+  });
+
+  console.log(uf.isInSameSet(0, 0));
+  console.log(uf.isInSameSet(0, 1));
+  /**
+   * UnionFind { parentOf: [ 2, 3, 2, 3 ], sizeOf: [ 1, 1, 2, 2 ] }
+   * 0 is under group 2
+   * 1 is under group 3
+   * 0 is under group 2
+   * 1 is under group 3
+   * true
+   * false
+   */
 }
